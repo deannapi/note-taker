4e5358122f40c94b8200181c1e67922aaf6633cb
deannapi <deannapi.mart@gmail.com>
2020-05-29 20:24:12 -0500

    changed post request

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,56 +1,61 @@
 // const notesData = require("../data/notesData");
 const fs = require("fs");
 const util = require("util");
+const app = require("express").Router();
 const writeFileAsync = util.promisify(fs.writeFile);
 const readFileAsync = util.promisify(fs.readFile);
 var notesData;
 
-module.exports = function(app) { 
-    // GET request
-    app.get("/api/notes", (req, res) => {
-        // Reads the notes from JSON file
-        readFileAsync("../db/db.json", "utf8").then(function (data) {
-            // Parse data to get an array of objects
-            notesData = JSON.parse(data);
-            // 
-            res.json(notesData);
-        });
+//module.exports = function(app) {
+// GET request
+app.get("/notes", (req, res) => {
+  // Reads the notes from JSON file
+  readFileAsync("db/db.json", "utf8").then(function (data) {
+    // Parse data to get an array of objects
+    notesData = JSON.parse(data);
+    //
+    res.json(notesData);
+  });
+});
+
+// POST  request
+app.post("/notes", (req, res) => {
+  readFileAsync("db/db.json", "utf8").then(function (data) {
+    // Parse data to get an array of objects
+    notesData = JSON.parse(data);
+
+    let newNote = req.body;
+    let currentID = notesData.length;
+
+    newNote.id = currentID + 1;
+    // Add new note to the array of note objects
+    notesData.push(newNote);
+
+    notesData = JSON.stringify(notesData);
+
+    writeFileAsync("db/db.json", notesData).then(function (data) {
+      console.log("Note has been added.");
     });
+    res.json(notesData);
+  });
+});
 
-    // POST  request
-    app.post("/api/notes", (req, res) => {
-        //  Add a 1 to the ID for each new note
-        let newNote = req.body;
-        let currentID = 0;
-        if (notesData.length !== 0) {
-            currentID = notesData[notesData.length -1]["id"];
-        }
-        let newID = currentID +1;
-        newNote["id"] = newID;
-        console.log(req.body);
-        // Add new note to the array of note objects
-        notesData.push(newNote);
-        //  Writes the new note to the file
-        writeFileAsync("../db/db.json", "utf8").then(function (data) {
-            console.log("Note has been added.");
-            });
-            res.json(newNote);
-        });
+// DELETE request
+app.delete("/notes/:id", (req, res) => {
+  let selID = parseInt(req.params.id);
+  //  Read JSON file
+  for (let i = 0; i < notesData.length; i++) {
+    if (selID === notesData[i].id) {
+      notesData.splice(i, 1);
+      let noteJSON = JSON.stringify(notesData, null, 2);
 
-    // DELETE request
-    app.delete("/api/notes/:id", (req, res) => {
-        let selID = parseInt(req.params.id);
-        //  Read JSON file
-        for (let i = 0; i < notesData.length; i++) {
-            if (selID === notesData[i].id) {
-                notesData.splice(i, 1);
-                let noteJSON = JSON.stringify(notesData, null, 2);
+      writeFileAsync("db/db.json", noteJSON).then(function () {
+        console.log("Note has been deleted.");
+      });
+    }
+  }
+  res.json(notesData);
+});
+//};
 
-                writeFileAsync("../db/db.json", noteJSON).then(function () {
-                    console.log("Note has been deleted.");
-                });
-            }
-        }
-        res.json(notesData);
-    });
-};
+module.exports = app;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 const express = require('express');
 const app = express();
+const apiRoutes = require('./routes/apiRoutes');
 const PORT = process.env.PORT || 3003;
 
 // Folder to retrieve CSS and JS Files
